50f607be3290765d9ad2063d7133da3f9dd4ddc2
ja-taleon <126250831+ja-taleon@users.noreply.github.com>
2023-02-28 19:12:05 +0800

    Update FoodMenu.java

--- FoodMenu.java
+++ FoodMenu.java
@@ -1,93 +1,78 @@
 package foodmenu;
 import java.util.*;
-
 public class FoodMenu {
+    private String name = "";
+    private double price = 0.00;
     
-    public Map <String, Double> items;
-    
-    public FoodMenu(){
-        items = new HashMap<>();
-        
+    public FoodMenu(String n, double p){
+        this.name = n;
+        this.price = p;
     }
     
-    public void addItem(String n, double p){
-        
-        if(items.containsKey(n)){
-            System.out.println("Error: " +  n + " is already on the menu");
-        }else{
-            items.put(n, p);
-            System.out.println("Added item : " + n + " to the menu for $ : " + p  );
-        
+    public void addItems(String n, double p){
+       
+            if(n.equals(name)){
+                System.out.println("Error: " + n + " is already on the menu");
+            }else{
+                System.out.println("Added: " + n + " to the menu for the price of: " + p);
+            }
         }
-    }
     
-    public void editItem(String n, double p){
-        
-        if (items.containsKey(n)){
-            items.put(n, p);
-        System.out.println("Changed price of : " + n + " to $ : " + p);
+    public void editPrice(String n, double p){
+        if(n.equals(name)){
+            price = p;
+            System.out.println("Edited: " + n + "'s prive to: " + p);
         }else{
-            System.out.println("Error: " +  n + " is not on the menu");
-        }  
+            System.out.println("Error: " + n + " is not on the menu");
+        }
     }
     
     public void displayMenu(){
-        
-        if(items.isEmpty()){
+        if(name.equals("")){
             System.out.println("Error: The menu is empty");
         }else{
-        System.out.println("==={Current Men}===: ");
-        for(Map.Entry<String, Double>entry : items.entrySet()){
-        System.out.println(entry.getKey() + " $: " + entry.getValue());
+        System.out.println("==={Current Menu}===: ");
+        System.out.println(name + ": $" + price);
+        System.out.println("====================: ");
         }
         }
-    }
 
     public static void main(String[] args) {
-        
         Scanner scan = new Scanner(System.in);
-        FoodMenu menu = new FoodMenu();
-        boolean running = true;
-        
-        while(running){
+        FoodMenu menu = new FoodMenu("", 0.00);
         System.out.println("==={Menu}===");
         System.out.println("1. Insert");
         System.out.println("2. Edit");
         System.out.println("3. View");
         System.out.println("4. Exit");
         System.out.println("============");
-        
         int MenuChoices = scan.nextInt();
         
         switch(MenuChoices){
-            
-            case 1 -> {
+            case 1:
                 System.out.println("============");
                 System.out.println("Enter new menu item name:  ");
-                String NewName = scan.next();
+                String newName = scan.next();
                 System.out.println("Enter new menu item price: ");
-                double NewPrice = scan.nextDouble();
-                menu.addItem(NewName, NewPrice);
-                }
-               
-            case 2 -> {
+                double newPrice = scan.nextDouble();
+                menu.addItems(newName, newPrice);
+                break;
+            case 2:
                 System.out.println("============");
                 System.out.println("Enter menu item name:  ");
-                String EditedName = scan.next();
+                String itemName = scan.next();
                 System.out.println("Enter new menu item price: ");
-                double EditedPrice = scan.nextDouble();
-                menu.editItem(EditedName, EditedPrice);
-                }
-                
-            case 3 -> menu.displayMenu();
-                
-            case 4 -> {
-                System.out.println("Closing Menu, Thank You");
-                running = false;
+                double editedPrice = scan.nextDouble();
+                menu.editPrice(itemName,editedPrice);
                 break;
-                }
-            default -> System.out.println("Error: Invalid Choice, Try Again");
-        }
-        }
+            case 3:
+                menu.displayMenu();
+                break;
+            case 4:
+                System.out.println("Closing Menu, Thank You");
+                break;
+                default:
+                System.out.println("Error: Invalid Choice, Try Again");
+        }    
     }
 }
