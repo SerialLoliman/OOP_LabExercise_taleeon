8ab5960397c9e1e69a074cd8f1951a8aa1b6a354
ja-taleon <126250831+ja-taleon@users.noreply.github.com>
2023-05-26 18:59:47 +0800

    Add files via upload

---
+++ src/entity/Entity.java
@@ -0,0 +1,118 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import main.GamePanel;
+import main.UtilityTool;
+
+public class Entity {
+    
+    GamePanel gp;
+    
+    public int worldX, worldY;
+    public int speed;
+    
+    public BufferedImage up1, up2,up3, down1, down2, down3, left1, left2, left3, right1, right2, right3;
+    public String direction;
+    
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    public int solidAreaDefaultX, solidAreaDefaultY;
+    public boolean collisionOn = false;
+    public int actionLockCounter = 0;
+    
+    public Entity (GamePanel gp){
+        this.gp = gp;
+    }
+    
+    public void setAction(){
+        
+    }
+    public void update(){
+        
+        setAction();
+        
+        collisionOn = false;
+        gp.cChecker.checkTile(this);
+        gp.cChecker.checkObject(this, false);
+        gp.cChecker.checkPlayer(this);
+
+        //IF COLLISION IS FALSE, PLAYER CAN MOVE
+        if(collisionOn == false){
+        switch(direction){
+            case "up":worldY -= speed;break;
+            case "down":worldY += speed;break;
+            case "left":worldX -= speed;break;
+            case "right":worldX += speed;break;
+        }
+        }
+        
+    spriteCounter ++;
+    if(spriteCounter >10){
+        if(spriteNum == 1){spriteNum = 2;}
+        else if(spriteNum ==2){spriteNum = 3;}
+        else if(spriteNum ==3){spriteNum = 1;}
+        spriteCounter = 0;}
+        
+    }
+    
+    public void draw(Graphics2D g2){ //FOR NPC
+        
+        BufferedImage image = null;
+        
+            int screenX = worldX - gp.player.worldX + gp.player.screenX;
+            int screenY = worldY - gp.player.worldY + gp.player.screenY;
+            
+            
+            if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && 
+               worldX - gp.tileSize < gp.player.worldX + gp.player.screenX && 
+               worldY + gp.tileSize > gp.player.worldY - gp.player.screenY && 
+               worldY - gp.tileSize < gp.player.worldY + gp.player.screenY){
+            
+            switch(direction){
+        
+            case "up":
+                if(spriteNum == 1){image = up1;}
+                if(spriteNum == 2){image = up2;}
+                if(spriteNum == 3){image = up3;}
+                break;
+            case "down":
+                if(spriteNum == 1){image = down1;}
+                if(spriteNum == 2){image = down2;}
+                if(spriteNum == 3){image = down3;}
+                break;
+            case "left":
+                if(spriteNum == 1){image = left1;}
+                if(spriteNum == 2){image = left2;}
+                if(spriteNum == 3){image = left3;}
+                break;
+            case "right":
+                if(spriteNum == 1){image = right1;}
+                if(spriteNum == 2){image = right2;}
+                if(spriteNum == 3){image = right3;}
+                break;
+            
+        }
+                
+               g2.drawImage(image,screenX,screenY,null);
+            }
+    }
+        public BufferedImage setup(String imageName){
+    
+        UtilityTool uTool = new UtilityTool();
+        BufferedImage image = null;
+        
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/res/oldman/char1/"+ imageName + ".png"));
+            image = uTool.scaleImage(image,gp.tileSize,gp.tileSize);
+              
+        }catch(IOException e){
+        e.printStackTrace();
+        }
+        return image;
+    }
+}
