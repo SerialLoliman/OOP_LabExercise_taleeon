3657eb2837e50830f82882eb69bad013c2540390
ja-taleon <126250831+ja-taleon@users.noreply.github.com>
2023-06-10 18:57:21 +0800

    Add files via upload

---
+++ src/main/KeyHandler.java
@@ -0,0 +1,230 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class KeyHandler implements KeyListener{
+    
+    GamePanel gp;
+    public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed, spacePressed;
+    
+    //DEBUG
+    boolean checkDrawTime = false;
+    
+    
+    public KeyHandler(GamePanel gp){
+        this.gp = gp;
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e){}
+    @Override
+    public void keyPressed(KeyEvent e){
+    
+        int code = e.getKeyCode();
+        
+        //TITLE STATE
+        if(gp.gameState == gp.titleState){
+            titleState(code);     
+        }
+        //PLAY STATE
+        else if(gp.gameState == gp.playState){
+            playState(code); 
+        }
+        //PAUSE STATE
+        else if(gp.gameState == gp.pauseState){
+            pauseState(code); 
+        }
+        //DIALOGUE STATE
+        else if(gp.gameState == gp.dialogueState){
+            dialogueState(code); 
+        }
+        //CHARACTER STATE
+        else if(gp.gameState == gp.characterState){
+            characterState(code); 
+        }  
+    }
+    
+    public void titleState(int code){
+            if(code == KeyEvent.VK_UP){
+            gp.ui.commandNum--;
+            if(gp.ui.commandNum < 0){
+                gp.ui.commandNum = 2;
+            }
+        }
+        if(code == KeyEvent.VK_DOWN){
+            gp.ui.commandNum++;
+            if(gp.ui.commandNum > 2){
+                gp.ui.commandNum = 0;
+            }
+        }
+        
+        if(code == KeyEvent.VK_SPACE){
+            //NEW GAME
+            if(gp.ui.commandNum == 0){
+                gp.gameState = gp.playState;
+            }
+            //LOAD GAME
+            if(gp.ui.commandNum == 1){
+                //to be added
+            }
+            //QUIT TO DESKTOP
+            if(gp.ui.commandNum == 2){
+                System.exit(0);
+            }
+        }
+    }
+    public void playState(int code){
+            if(code == KeyEvent.VK_UP){
+            upPressed = true;
+        }
+        
+        if(code == KeyEvent.VK_DOWN){
+            downPressed = true;
+        }
+        
+        if(code == KeyEvent.VK_LEFT){
+            leftPressed = true;
+        }
+        
+        if(code == KeyEvent.VK_RIGHT){
+            rightPressed = true;
+        }
+        
+        //PAUSE
+        if(code == KeyEvent.VK_P){
+            gp.gameState = gp.pauseState;
+        }
+        
+        //START DIALOGUE
+        if(code == KeyEvent.VK_SPACE){
+            spacePressed = true;
+        }
+        
+        //OPEN CHARACTER STATS
+        if(code == KeyEvent.VK_I){
+            gp.gameState = gp.characterState;
+        }
+        
+        
+        //DEBUG
+        if(code == KeyEvent.VK_T){
+            if(checkDrawTime == false){
+                checkDrawTime = true;
+            }
+            else if (checkDrawTime == true){
+            checkDrawTime = false;
+            }
+        }
+    }
+    public void pauseState(int code){
+            if(code == KeyEvent.VK_P){
+            gp.gameState = gp.playState;
+            }
+    }
+    public void dialogueState(int code){
+            if (code == KeyEvent.VK_SPACE){
+            gp.gameState = gp.playState;
+            }
+    }
+    public void characterState(int code){
+        
+        //INVENTORY
+        if(code == KeyEvent.VK_I){
+        gp.gameState = gp.playState;
+        }
+        
+        //MOVE INVENTORY CURSOR
+        if(code == KeyEvent.VK_UP){
+            if(gp.ui.slotRow != 0){
+            gp.ui.slotRow--;
+            }
+        }
+        
+        if(code == KeyEvent.VK_DOWN){
+            if(gp.ui.slotRow != 3){
+            gp.ui.slotRow++;
+            }
+        }
+        
+        if(code == KeyEvent.VK_LEFT){
+            if(gp.ui.slotCol != 0){
+            gp.ui.slotCol--;
+            }
+        }
+        
+        if(code == KeyEvent.VK_RIGHT){
+            if(gp.ui.slotCol != 4){
+            gp.ui.slotCol++;
+            }
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e){
+    
+        
+        int code = e.getKeyCode();
+        
+        if(code == KeyEvent.VK_UP){
+            upPressed = false;
+        }
+        
+        if(code == KeyEvent.VK_DOWN){
+            downPressed = false;
+        }
+        
+        if(code == KeyEvent.VK_LEFT){
+            leftPressed = false;
+        }
+        
+        if(code == KeyEvent.VK_RIGHT){
+            rightPressed = false;
+        }
+        if(code == KeyEvent.VK_ENTER){
+            enterPressed = false;
+        }
+        if(code == KeyEvent.VK_SPACE){
+            spacePressed = false;
+        }
+    }
+    
+}
+
+
+//CHARACTER SELECT       
+//        else if(gp.ui.titleScreenState == 1){
+//        if(code == KeyEvent.VK_UP){
+//            gp.ui.commandNum--;
+//            if(gp.ui.commandNum < 0){
+//                gp.ui.commandNum = 3;
+//            }
+//        }
+//        if(code == KeyEvent.VK_DOWN){
+//            gp.ui.commandNum++;
+//            if(gp.ui.commandNum > 3){
+//                gp.ui.commandNum = 0;
+//            }
+//        }
+//        
+//        if(code == KeyEvent.VK_SPACE){
+//            if(gp.ui.commandNum == 0){
+//                System.out.println("GREEN SLIME");
+//                gp.gameState = gp.playState;
+//                gp.playMusic(0);
+//            }            
+//            if(gp.ui.commandNum == 1){
+//                System.out.println("BLUE SLIME");
+//                gp.gameState = gp.playState;
+//                gp.playMusic(0);
+//            }
+//            if(gp.ui.commandNum == 2){
+//                System.out.println("RED SLIME");
+//                gp.gameState = gp.playState;
+//                gp.playMusic(0);
+//            }
+//            if(gp.ui.commandNum == 3){
+//                gp.ui.titleScreenState = 0;
+//            }
+//        }          
+//        }
